88acf9cbc50dba673d81c9bb9f42cee29690420d
Donovan <lexi@Lexi>
2020-04-29 14:03:40 +0100

    UMAP is awesome!

--- refac/main.py
+++ refac/main.py
@@ -7,19 +7,24 @@ class Config():
     def __init__(self):
         self.dset = 'testlocal'
         self.num = 16*10
-        self.snr = 1000
+        self.snr = 1/4
         self.ds = 1
-        self.num_comps = 2
+        self.num_comps = 3
         self.model = 'UMAP'
 
 
-def plot(lines_reddim):
+def plot(lines_reddim, num):
     import matplotlib.pyplot as plt
-    plt.scatter(lines_reddim[:, 0], lines_reddim[:, 1])
+    per_sino = lines_reddim.shape[0] // num
+    for x in range(num):
+        if x % 2 == 0:
+            plt.scatter(lines_reddim[x*per_sino:(x+1)*per_sino, 0], lines_reddim[x*per_sino:(x+1)*per_sino:, 1], c='r')
+        else:
+            plt.scatter(lines_reddim[x*per_sino:(x+1)*per_sino, 0], lines_reddim[x*per_sino:(x+1)*per_sino:, 1], c='b')
     plt.show()
 
 
 sinos = sinogram_main(Config())
 lines_reddim = fitmodel(sinos, Config().model, Config().num_comps)
-plot(lines_reddim)
+plot(lines_reddim, Config().num)
 clustering_main(lines_reddim, Config())
